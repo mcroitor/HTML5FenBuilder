3c5d4d1f62e84037693750dc68bc6f58a78c463b
Root <Root@139-Root>
2013-10-02 11:09:56 +0300

    some fixes

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -7,8 +7,8 @@ var font = fonts[0];
 var fen = "8/8/8/8/8/8/8/8";
 var lines = chars[font].empty;
 var charTool = [
-    ["k", "q", "r", "b", "n", "p"],
-    ["l", "w", "t", "v", "m", "o"]
+    ["k", "q", "r", "b", "n", "p", " "],
+    ["l", "w", "t", "v", "m", "o", " "]
 ];
 var actionPanel;
 var selected = null;
@@ -32,17 +32,17 @@ function init() {
 
 function initActionPanel() {
     actionPanel = _id("actionPanel");
-    actionPanel.setAttribute("width", 30 * 6 + 2);
+    actionPanel.setAttribute("width", 30 * 7 + 2);
     actionPanel.setAttribute("height", 30 * 2 + 2);
     cd = actionPanel.getContext("2d");
     cd.fillStyle = "#000000";
-    cd.fillRect(0, 0, 30 * 6 + 2, 30 * 2 + 2);
+    cd.fillRect(0, 0, 30 * 7 + 2, 30 * 2 + 2);
     cd.fillStyle = "#ffffff";
-    cd.fillRect(1, 1, 30 * 6, 30 * 2);
+    cd.fillRect(1, 1, 30 * 7, 30 * 2);
     cd.fillStyle = "#000000";
     cd.font = "30px " + fonts[0];
-    cd.fillText(charTool[0].join(""), 1, 31, 30 * 6);
-    cd.fillText(charTool[1].join(""), 1, 61, 30 * 6);
+    cd.fillText(charTool[0].join(""), 1, 31, 30 * 7);
+    cd.fillText(charTool[1].join(""), 1, 61, 30 * 7);
 }
 
 function generateImage() {
@@ -70,10 +70,10 @@ function mouseClick(event) {
     var x = Math.floor((event.pageX - canvas.offsetLeft) / fieldSize);
     var y = Math.floor((event.pageY - canvas.offsetTop) / fieldSize);
 
-    if (selected != null && y > 0 && x > 0 && y < 10 && x < 10) {
+    if (selected !== null && y > 0 && x > 0 && y < 10 && x < 10) {
         board[y - 1][x - 1] = selected;
         updateFen();
-        generateImage();
+        setBoard();
     }
 
 }
@@ -100,7 +100,7 @@ function setBoard() {
             }
             else {
                 var tmp = l[i][j].toString() + (i + k) % 2;
-                pos[i][k] = l[i][j];
+                board[i][k] = l[i][j];
                 k++;
             }
         }
@@ -133,7 +133,7 @@ function updateFen() {
         count = 0;
         for (j = 0; j < 8; ++j)
         {
-            if (board[i][j] == 0)
+            if (board[i][j] === ' ' )
             {
                 count++;
             }
@@ -159,7 +159,11 @@ function board2Lines(font) {
     for (var j = 0; j !== 8; ++j) {
         for (var i = 0; i !== 8; ++i) {
             if (board[j][i] !== ' ') {
-                var code = board[j][i] + (i + j) % 2;
+                var code = board[j][i] + (i + j+1) % 2;
+                result[j + 1] = result[j + 1].substr(0, i + 1) + chars[font][code] + result[j + 1].substr(i + 2, 10);
+            }
+            else{
+                var code = ((i + j) % 2)? "light" : "dark";
                 result[j + 1] = result[j + 1].substr(0, i + 1) + chars[font][code] + result[j + 1].substr(i + 2, 10);
             }
         }
